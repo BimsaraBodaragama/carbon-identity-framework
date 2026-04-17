86f338c87da62b4f06b9af95bf231125019f76e7
Kavinda Rajapakse <kavindasr@gmail.com>
2026-04-17 12:41:11 +0530

    Add regex import for enhanced validation capabilities

--- components/user-store/org.wso2.carbon.identity.user.store.configuration/src/main/java/org/wso2/carbon/identity/user/store/configuration/UserStoreConfigAdminService.java
+++ components/user-store/org.wso2.carbon.identity.user.store.configuration/src/main/java/org/wso2/carbon/identity/user/store/configuration/UserStoreConfigAdminService.java
@@ -42,6 +42,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.xml.transform.TransformerConfigurationException;
 
